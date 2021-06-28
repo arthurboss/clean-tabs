1ab744e8085d2d483fe05029d3838cbf7b80de6b
Arthur Boss <arthurboss@outlook.com>
2021-06-28 03:21:48 +0200

    style(tab): render title separately

--- src/components/tab/index.tsx
+++ src/components/tab/index.tsx
@@ -2,6 +2,21 @@ import React from 'react';
 
 type TTab = { src: string };
 
-const Tab = ({ src }: TTab) => <pre>{src}</pre>;
+const Tab = ({ src }: TTab) => {
+  const title = src.split('\n')[0];
+
+  const tab = src
+    .split('\n')
+    .slice(1)
+    .join('\n')
+    .replace(/(^\s*(?!.+)\n+)|(\n+\s+(?!.+)$)/g, '');
+
+  return (
+    <>
+      <h2>{title}</h2>
+      <pre>{tab}</pre>
+    </>
+  );
+};
 
 export default Tab;
